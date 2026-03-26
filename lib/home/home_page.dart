6a60b721593597d4b9667ab18b766d3e1d144a4d
salmoorovurmat <salmoorov@gmail.com>
2026-03-26 16:34:39 +0600

    second homework 2

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -12,7 +12,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-final  List<Todo> _tasks= [];
+  final List<Todo> _tasks = [];
 
   void _naviateToAddPage() async {
     final result = await Navigator.push(
@@ -21,12 +21,12 @@ final  List<Todo> _tasks= [];
     );
     if (result != null && result.toString().isNotEmpty) {
       setState(() {
-    _tasks.add(Todo(title: result.toString(), date: DateTime.now())); 
+        _tasks.add(
+          Todo(title: result.toString(), date: DateTime.now(), id: 0 + 1),
+        );
       });
     }
   }
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -34,61 +34,63 @@ final  List<Todo> _tasks= [];
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
-      ), body: Container(
-        constraints: BoxConstraints(maxHeight: 200),
-        decoration: BoxDecoration(color:  const Color.fromARGB(255, 4, 187, 83),
-        borderRadius: BorderRadius.circular(12),
-        ),  
-        
-        child: ListView.separated( 
-              padding: const EdgeInsets.all(16),
-              shrinkWrap: true,
-              itemCount: _tasks.length,
-              
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(_tasks[index].title),
-                  leading: const Icon(Icons.check_box_outline_blank,
-                  color: Colors.black,),
-                  
-                  
-                ); 
-               
-              },  
-              separatorBuilder: (context, index) => const SizedBox(height: 12), 
-            ), 
-             
-      ),  
-      
-      bottomNavigationBar: Container(
-        padding: EdgeInsets.symmetric(
-          horizontal: 16, vertical: 10
-        ), 
-     
-        width:  double.infinity,
-        height: 56,
-        child: ElevatedButton(style: ElevatedButton.styleFrom(
-          backgroundColor: Colors.blue,
-          shape: RoundedRectangleBorder(borderRadius: BorderRadiusGeometry.circular(9)),
-          elevation: 0,
-        ),
-        
-        onPressed: _naviateToAddPage,
-        
-        child: const Icon(Icons.add),
-        
       ),
+      body: Container(
+        constraints: BoxConstraints(maxHeight: 200),
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(255, 4, 187, 83),
+          borderRadius: BorderRadius.circular(12),
+        ),
+
+        child: ListView.separated(
+          padding: const EdgeInsets.all(16),
+          shrinkWrap: true,
+          itemCount: _tasks.length,
+
+          itemBuilder: (context, index) {
+            final dateTime = _tasks[index].date;
+            return ListTile(
+              onTap: () {
+                print(_tasks[index].id);
+              },
+              title: Text(_tasks[index].title),
+              leading: const Icon(
+                Icons.check_box_outline_blank,
+                color: Colors.black,
+              ),
+              trailing: Text(dateTime.toString()),
+            );
+          },
+          separatorBuilder: (context, index) => const SizedBox(height: 12),
+        ),
+      ),
+
+      bottomNavigationBar: Container(
+        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+
+        width: double.infinity,
+        height: 56,
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.blue,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadiusGeometry.circular(9),
+            ),
+            elevation: 0,
+          ),
+
+          onPressed: _naviateToAddPage,
+
+          child: const Icon(Icons.add),
+        ),
       ),
     );
-    
   }
-   // void _naviateToAddPage()async{
+  // void _naviateToAddPage()async{
 
-     // final result  = await Navigator.push(context, MaterialPageRoute(builder: (_) => AddPage() ));
-      //if (result != null){
-        //print ('распечатать: $result');
-      //}
-    //}
-  }
-
-
+  // final result  = await Navigator.push(context, MaterialPageRoute(builder: (_) => AddPage() ));
+  //if (result != null){
+  //print ('распечатать: $result');
+  //}
+  //}
+}
